d2cea41c4aeb4553bfcdfd73d0ca257f9089341c
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-11-01 16:42:51 -0400

    commit tree 42b31576865860cd28d9a2b32ec41937af3d19eb

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution here
+//const mydrivers = ['Bobby','Sally','Joe', 'bobby']
+
+function findMatching(arrayofdrivernames, driverwearelookingfor){
+    const result = arrayofdrivernames.filter(name => name.toLowerCase()=== driverwearelookingfor.toLowerCase())
+    console.log (result);
+return (result)
+}
+
+//findMatching(mydrivers, 'Bobby')
+
+function fuzzyMatch(drivers, string){
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+return result;
+}
+
+
+const myarrayofobjects = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function matchName(arrayofdriverobjects,string){
+    const result = arrayofdriverobjects.filter(driver =>driver.name === string);
+    console.log (result)
+    return result;
+}
+
+matchName(myarrayofobjects,'Bobby')
